9576cdc178db052fdc7160405df59d9a21cb36ae
Sara Elnaggar <sara.elnaggar.114@gmail.com>
2024-08-22 16:10:21 +0300

    Initial Commit

---
+++ WeatherCastApp/WeatherCastAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCastAppApp.swift
+//  WeatherCastApp
+//
+//  Created by Sarah on 22/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCastAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
